d80c2b1aafde58de94ea1c4dcbabe7bc05b0af7d
Alex Morozov <morozov@podmoskovie.info>
2013-12-23 10:11:17 +0400

    Django django.conf.urls.defaults module is deprecated.

--- reversion_compare/admin.py
+++ reversion_compare/admin.py
@@ -12,7 +12,10 @@
 import logging
 
 from django import template
-from django.conf.urls.defaults import patterns, url
+try:
+    from django.conf.urls.defaults import patterns, url
+except ImportError:
+    from django.conf.urls import patterns, url
 from django.contrib.admin.util import unquote, quote
 from django.core.urlresolvers import reverse
 from django.http import Http404
